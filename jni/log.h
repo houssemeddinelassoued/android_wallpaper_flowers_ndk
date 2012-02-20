d8a0d0dbd71a3dc7829598fe6b07e093aa566999
harism <harism@gmail.com>
2012-02-20 19:04:18 +0200

    Comments added.

--- jni/log.h
+++ jni/log.h
@@ -1,6 +1,23 @@
+/*
+ Copyright 2012 Harri Smått
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
 #ifndef LOG_H__
 #define LOG_H__
 
+// TODO: It may be best to use some debug flag instead.
 #if 1
 #include <android/log.h>
 #define LOGD(tag, ...) __android_log_print(ANDROID_LOG_DEBUG, tag, __VA_ARGS__)
